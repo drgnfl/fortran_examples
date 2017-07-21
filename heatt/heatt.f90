cc23a0fcb2107425a8c03f137e6eb4a0114d2b4a
drgnfl <pablo.cardenasz@alumnos.usm.cl>
2017-07-21 19:08:24 -0400

    graph w image and contours

--- heatt/heatt.f90
+++ heatt/heatt.f90
@@ -10,6 +10,7 @@ double precision dx,y,x, T(N,N), qx, qy, magn
 dx = L/(N-1) !Espacio entre nodos
 !Escritura de Tabla
 !El formato es pos_x pos_y valor
+!Notar el espacio entre cada fila (grided data)
 open(unit=10,file='heatt.txt')
 do i=1,N
 	y=dx*(i-1)
@@ -18,6 +19,7 @@ do i=1,N
 		T(i,j) = sinh(3.14*(L-x)/L)**0.5*sin(3.14*y/L)*y
 		write(10,*) x,y, T(i,j)
 	enddo
+	write(10,*) 
 enddo
 close(10)
 !Vectores del gradiente
@@ -33,7 +35,7 @@ do i=2,N-1
 	enddo
 enddo
 close(20)
-!Ploteo llamando a gnuplot
+!Ploteo llamando a gnuplot con círculos
 open(unit=40, file="style.gnu")
 write(40,*) "set terminal pngcairo enhanced font 'Verdana,10'"
 write(40,*) "set output 'heatt.png'"
@@ -47,9 +49,31 @@ write(40,*) "plot 'heatt.txt' u 1:2:3 w circles palette"
 write(40,*) "set output 'heattvec.png'"
 write(40,*) "plot 'heattvec.txt' u 1:2:3:4:5 w vectors filled head palette"
 close(unit=40)
-!Se borran los archivos auxiliares
 call system('gnuplot style.gnu')
+!Ploteo llamando a gnuplot con círculos
+open(unit=50, file="style2.gnu")
+write(50,*) "set terminal pngcairo enhanced font 'Verdana,10'"
+write(50,*) "set output 'heatt2.png'"
+write(50,*) "unset key"
+write(50,*) "set xrange [0:1]"
+write(50,*) "set yrange [0:1]"
+write(50,*) "set palette defined(1 'blue',2 'green',3 'yellow',4 'red')"
+write(50,*) "set contour"
+write(50,*) "unset surface"
+write(50,*) "set cntrparam levels auto 20"
+write(50,*) "set view map"
+write(50,*) "unset clabel"
+write(50,*) "set table 'cont.dat'"
+write(50,*) "splot 'heatt.txt' u 1:2:3 w lines"
+write(50,*) "unset table"
+write(50,*) "unset contour"
+write(50,*) "plot 'heatt.txt' u 1:2:3 w image, 'cont.dat' w l lt -1 lw 1.5"
+close(unit=50)
+call system('gnuplot style2.gnu')
+!Se borran los archivos auxiliares
 call system('rm style.gnu')
+call system('rm style2.gnu')
 call system('rm heatt.txt')
+call system('rm cont.dat')
 call system('rm heattvec.txt')
 end program
